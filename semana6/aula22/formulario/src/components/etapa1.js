a9024b1a14725d7320059881f5449ac51e34bf0b
Ntfigueira <ntfigueira2@gmail.com>
2021-03-15 17:21:19 -0300

    Formulário

---
+++ semana6/aula22/formulario/src/components/etapa1.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Etapa1 extends React.Component {
+    render()
+    return(
+        <div>
+    <h2> Etapa 1 - Dados Gerais</h2>
+    <PerguntaCampoBranco pergunta={"1. Qual o seu nome?"}
+    <PerguntaCampoBranco pergunta={"1. Qual a sua idade?"}
+    <PerguntaCampoBranco pergunta={"1. Qual o seu endereço?"}
+    <PerguntaCampoBranco pergunta={"1. Qual o seu nome?"}
+            
+    )
+}

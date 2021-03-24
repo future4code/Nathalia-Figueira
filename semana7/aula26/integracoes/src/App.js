620199a42f3a6ee702bc423389b320e8e4e4ee20
Ntfigueira <ntfigueira2@gmail.com>
2021-03-23 21:58:39 -0300

    integrações

--- semana7/aula26/integracoes/src/App.js
+++ semana7/aula26/integracoes/src/App.js
@@ -1,86 +1,104 @@
 import React from 'react';
-import style from 'styled-components';
-import axios from 'axios'
+import styled from 'styled-components';
+import axios from 'axios';
+
+
+const Box = styled.div`
+    margin: 10% 40%;
+    display: flex;
+    background-color: grey; 
+    flex-direction: column;
+    border: 2px solid black;
+    border-radius: 10px;
+    justify-content: center;
+    align-items:center;
+    height: 300px;
+    width: 20%;`
+
+const Itens = styled.div`
+margin: 10px;
+justify-content: space-around;
+`
+const Item = styled.div`
+margin: 4px;`
+
 
 export default class App extends React.Component {
   state = {
-    user: [""],
+    id: [],
     inputName: "",
-    inputEmail: "",
-  };
-
-  componentDidMoount() {
-    this.getUserlist();
+    inputEmail:"",
 
   }
-  handleInputChange = (e) => {
-    this.setState({ inputName: e.target.value });
-    this.setState({ inputEmail: e.target.value });
+  handleOnChangeName = (event) => {
+    this.setState({ inputName: event.target.value });
   };
-  getUserlist = () => {
+  handleOnChangeEmail = (event) => {
+    this.setState({ inputEmail: event.target.value })
+  }
+  createUser = () => {
+    const body = {
+      name: this.state.inputName,
+      email: this.state.inputEmail
+    };
     axios
-      .get(
-        `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/`,
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+        body,
         {
           headers: {
-            Authorization: "nathalia-figueira-cruz",
+            Authorization: "nathalia-figueira-cruz"
           }
         }
       )
       .then((res) => {
-        this.setState({ userlist: res.data.result.list });
-        console.log(res.data.result.list);
+        this.setState({ inputName: '' })
+        this.setState({ inputEmail: '' })
+        alert("Parabens!Cadastro realizado com Sucesso.")
       })
       .catch((err) => {
-        console.log(err.response.data);
+        this.setState({ inputName: '' })
+        this.setState({ inputEmail: '' })
+        alert("Por favor verifique as informações. </br>Nome ou Email Inválido.")
       });
-    userList = () => {
-      const body = {
-        name: this.state.inputName,
-        email: this.state.inputEmail,
-      };
-      axios
-        .post(
-          `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/:id`,
-            body,
-          {
-            headers: {
-              Authorization: "nathalia-figueira-cruz"
-            }
-          }
-        )
-        .then((res) => {
-          this.setState({ inputName: "" })
-          this.setState({ inputEmail: "" })
-          this.getUserlist()
-        })
-        .catch((err) => {
-          console.log(err.response.fata);
-        });
-    };
+  };
 
-    render()
-    {const userlistnew = this.state.userList.map((userlist) => (
-      <li key={userlist.id}> {userlist.name}{userlist.email}</li>
-      ));
-      return <div className="App">
-          <input
-            placeholder={"Nome:"}
-            value={this.state.inputName}
-            placeholder={"Email:"}
-            value={this.state.inputEmail}
-            onChange={this.handleInputChange}
-          />
-          <button onClick={this.createUserlist}>Criar Cadastro</button>
-          {this.state.userlist.length > 0 ? (
-            <ul>{userlistnew}</ul>
-          ) : (
-            <p>Carregando...</p>
-          )}
-        </div>;
+
+    render(){
+      return (
+        <div>
+          <button> Lista de cadastros</button>
+          <Box>
+            <h1>Cadastro</h1>
+            <Itens>
+                <Item>
+                  <label >Nome:</label>
+                </Item>
+                <Item>
+                  <input value={this.state.inputName}
+                    onChange={this.handleOnChangeName}
+                    placeholder={"Nome do Usuario"}>
+                  </input>
+                </Item>
+            </Itens>
+            <Itens>
+                <Item>
+                  <label >Email: </label>
+                </Item>
+                <Item>
+                  <input
+                    value={this.state.inputEmail}
+                    onChange={this.handleOnChangeEmail}
+                    placeholder={"Email"}>
+                  </input>
+                </Item> 
+            </Itens>
+            <button >Enviar</button>
+            </Box>
+        </div>
+      )
     }
-  }
-}
+    }
 
 
 

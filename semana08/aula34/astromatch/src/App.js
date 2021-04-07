47abb8bcff92b54706035fdc50c412c6948f32ae
Ntfigueira <ntfigueira2@gmail.com>
2021-04-07 17:08:15 -0300

    Inicio do projeto Astromatch

---
+++ semana08/aula34/astromatch/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {baseUrl} from './Parameters.js'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+}
+return
+export default App;
---
+++ semana08/aula34/astromatch/src/modules/Style.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {baseUrl} from './Parameters'
+import logo, { ReactComponent } from './logo.svg';
+import './App.css';
+
+
+export default class Style extends React.Component{
+
+
+    
+}

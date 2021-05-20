55790fd44bc07d5706c5c4ac9980f228ee6a8f5b
Nathália Figueira <ntfigueira2@gmail.com>
2021-05-20 06:58:57 -0300

    Exercico concluido ate o endpoint 2, no endpoint 3 tive problemas e nao consegui avançar

--- semana15/aula-43/express-template/src/index.ts
+++ semana15/aula-43/express-template/src/index.ts
@@ -1,10 +1,46 @@
-import express from "express"
+import express, {Request, Response} from "express"
 import cors from "cors"
+import {countries} from './countries'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+
+app.get("/test", (req: Request, resp: Response) => {
+    
+    const nome = req.body.nome
+    const cabelo = req.query.cabelo
+
+    resp.send({message: `Hello  ${nome}, seu cabelo é ${cabelo} `})
+})
+
+
+app.get ("/countries/all", (req: Request, res: Response) => { 
+    const countriesNameId = countries.map(country =>({
+        id: country.id,
+        name :country.name
+    }))
+
+    res
+    .status(200)
+    .send(countriesNameId)
+})
+
+
+
+app.get("/countries/:id",(req: Request, res: Response) => {
+    const id = Number(req.params.id)
+     
+     
+     if (id) {
+        res.status(200).send(countries.find((c=>c.id === id)))
+     } else {
+        res.status(404).send("Not found")
+     }
+
+})
 
 app.listen(3004, () => {
     console.log("server s running in http://localhost:3004")
